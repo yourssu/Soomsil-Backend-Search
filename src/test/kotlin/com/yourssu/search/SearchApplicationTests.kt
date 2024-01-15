0f56cec1d18c3f7bb23a9996ac412dc93c6ef7c5
shon5544 <sbs8239@gmail.com>
2024-01-15 17:25:42 +0900

    initial commit

---
+++ src/test/kotlin/com/yourssu/search/SearchApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.yourssu.search
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SearchApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
