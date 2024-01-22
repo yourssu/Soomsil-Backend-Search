94ae44fdf29523dfcaa4684337214cdfedc25aa0
sangcheol12 <eric2057@daum.net>
2024-01-22 17:20:16 +0900

    검색쿼리 pageable적용
    검색쿼리 pageable적용 및 테스트

--- src/main/kotlin/com/yourssu/search/crawling/repository/InformationRepository.kt
+++ src/main/kotlin/com/yourssu/search/crawling/repository/InformationRepository.kt
@@ -1,10 +1,12 @@
 package com.yourssu.search.crawling.repository
 
 import com.yourssu.search.crawling.domain.Information
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.elasticsearch.annotations.Query
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository
 
 interface InformationRepository : ElasticsearchRepository<Information, String> {
     @Query("{\"multi_match\":{\"query\":\"?0\",\"fields\":[\"title^7\",\"content^3\"],\"fuzziness\":\"AUTO\"}}")
-    fun findByInfoOrderByScoreDesc(query: String): List<Information>
+    fun findByInfoOrderByScoreDesc(query: String, pageable: Pageable): Page<Information>
 }
