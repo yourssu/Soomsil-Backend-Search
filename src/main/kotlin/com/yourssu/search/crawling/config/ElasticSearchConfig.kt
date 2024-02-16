66fe899646c65f8c46a1f5e856cb1e5e078f762f
shon5544 <sbs8239@gmail.com>
2024-02-16 21:10:05 +0900

    fix: 테스트 id, pw, endpoint 제거

--- src/main/kotlin/com/yourssu/search/crawling/config/ElasticSearchConfig.kt
+++ src/main/kotlin/com/yourssu/search/crawling/config/ElasticSearchConfig.kt
@@ -1,16 +1,22 @@
 package com.yourssu.search.crawling.config
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.elasticsearch.client.ClientConfiguration
 import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration
 
 @Configuration
-class ElasticSearchConfig : ElasticsearchConfiguration() {
-    override fun clientConfiguration(): ClientConfiguration {
-        val user = "elastic" // 초기 아이디
-        val password = "yourssuElasticsearch!" // 초기 테스트 비밀번호
-        val endpoint = "yourssu2024.cafe24.com:9200" // 초기 엔드 포인트
+class ElasticSearchConfig(
+    @Value("\${es.id}")
+    val user: String,
 
+    @Value("\${es.password}")
+    val password: String,
+
+    @Value("\${es.endpoint}")
+    val endpoint: String
+) : ElasticsearchConfiguration() {
+    override fun clientConfiguration(): ClientConfiguration {
         return ClientConfiguration.builder()
             .connectedTo(endpoint)
             .withBasicAuth(user, password)
