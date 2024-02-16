31654fdf21217a560b5aeb93a16594e2844123e1
shon5544 <sbs8239@gmail.com>
2024-02-16 21:04:34 +0900

    refactor: 안쓰는 모듈 import 참조 제거

--- src/main/kotlin/com/yourssu/search/crawling/config/ElasticSearchConfig.kt
+++ src/main/kotlin/com/yourssu/search/crawling/config/ElasticSearchConfig.kt
@@ -1,25 +1,14 @@
 package com.yourssu.search.crawling.config
 
-import org.apache.http.auth.AuthScope
-import org.apache.http.auth.UsernamePasswordCredentials
-import org.apache.http.impl.client.BasicCredentialsProvider
-import org.apache.http.impl.nio.client.HttpAsyncClientBuilder
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.elasticsearch.client.ClientConfiguration
-import org.springframework.data.elasticsearch.client.elc.ElasticsearchClients
 import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration
-import java.io.BufferedInputStream
-import java.io.FileInputStream
-import java.security.KeyStore
-import java.security.cert.CertificateFactory
-import javax.net.ssl.SSLContext
-import javax.net.ssl.TrustManagerFactory
 
 @Configuration
 class ElasticSearchConfig : ElasticsearchConfiguration() {
     override fun clientConfiguration(): ClientConfiguration {
         val user = "elastic"
-        val password = "yourssuElasticsearch!"
+        val password = "yourssuElasticsearch!" // 초기 테스트 비밀번호
 
         return ClientConfiguration.builder()
             .connectedTo("yourssu2024.cafe24.com:9200")
