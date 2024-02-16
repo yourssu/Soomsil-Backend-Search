de920ef85b21d62f903041931ddc50f24b00e48e
shon5544 <sbs8239@gmail.com>
2024-02-16 21:05:29 +0900

    refactor: 안쓰는 모듈 import 참조 제거

--- src/main/kotlin/com/yourssu/search/crawling/config/ElasticSearchConfig.kt
+++ src/main/kotlin/com/yourssu/search/crawling/config/ElasticSearchConfig.kt
@@ -7,11 +7,12 @@ import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfigurat
 @Configuration
 class ElasticSearchConfig : ElasticsearchConfiguration() {
     override fun clientConfiguration(): ClientConfiguration {
-        val user = "elastic"
+        val user = "elastic" // 초기 아이디
         val password = "yourssuElasticsearch!" // 초기 테스트 비밀번호
+        val endpoint = "yourssu2024.cafe24.com:9200" // 초기 엔드 포인트
 
         return ClientConfiguration.builder()
-            .connectedTo("yourssu2024.cafe24.com:9200")
+            .connectedTo(endpoint)
             .withBasicAuth(user, password)
             .build()
     }
