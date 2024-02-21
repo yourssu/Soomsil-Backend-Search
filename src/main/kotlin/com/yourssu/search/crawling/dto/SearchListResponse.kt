228b5fbf1a648ce1288a73214b71a91b1899577a
HeoSeokMun <tjrans303@soongsil.ac.kr>
2024-02-21 15:27:25 +0900

    feat: totalCount와 thumnailCount 추가

--- src/main/kotlin/com/yourssu/search/crawling/dto/SearchListResponse.kt
+++ src/main/kotlin/com/yourssu/search/crawling/dto/SearchListResponse.kt
@@ -1,7 +1,7 @@
 package com.yourssu.search.crawling.dto
 
 data class SearchListResponse(
+    val totalCount: Long,
     val resultCount: Int,
-    val resultList: List<SearchResponse>
-) {
-}
+    val resultList: List<SearchResponse>,
+)
--- src/main/kotlin/com/yourssu/search/crawling/service/SearchService.kt
+++ src/main/kotlin/com/yourssu/search/crawling/service/SearchService.kt
@@ -1,23 +1,28 @@
 package com.yourssu.search.crawling.service
 
-import com.yourssu.search.crawling.repository.InformationRepository
 import com.yourssu.search.crawling.dto.SearchListResponse
 import com.yourssu.search.crawling.dto.SearchResponse
 import com.yourssu.search.crawling.dto.SearchTopQuerysResponse
 import com.yourssu.search.crawling.repository.AccessLogNativeQueryRepository
+import com.yourssu.search.crawling.repository.InformationRepository
 import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
 
 @Service
 class SearchService(
     private val informationRepository: InformationRepository,
-    private val accessLogNativeQueryRepository: AccessLogNativeQueryRepository
+    private val accessLogNativeQueryRepository: AccessLogNativeQueryRepository,
 ) {
-    fun search(query: String, pageable: Pageable): SearchListResponse {
-        val informations = informationRepository.findByInfoOrderByScoreDesc(query, pageable).map { SearchResponse.of(it) }.toList()
+    fun search(
+        query: String,
+        pageable: Pageable,
+    ): SearchListResponse {
+        val result = informationRepository.findByInfoOrderByScoreDesc(query, pageable)
+        val informations = result.map { SearchResponse.of(it) }.toList()
         return SearchListResponse(
+            totalCount = result.totalElements,
             resultCount = informations.size,
-            resultList = informations
+            resultList = informations,
         )
     }
 
