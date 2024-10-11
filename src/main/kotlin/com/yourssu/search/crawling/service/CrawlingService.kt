49fd1e05dc96782c4e7fdd5d0ff73a2587824560
shon5544 <sbs8239@gmail.com>
2024-10-11 23:00:02 +0900

    refactor: 사용되지 않는 import 제거

--- src/main/kotlin/com/yourssu/search/crawling/service/CrawlingService.kt
+++ src/main/kotlin/com/yourssu/search/crawling/service/CrawlingService.kt
@@ -1,25 +1,9 @@
 package com.yourssu.search.crawling.service
 
-import com.yourssu.search.crawling.domain.Information
-import com.yourssu.search.crawling.domain.InformationUrl
-import com.yourssu.search.crawling.domain.SourceType
 import com.yourssu.search.crawling.repository.InformationRepository
-import com.yourssu.search.crawling.repository.InformationUrlRepository
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.async
-import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.withContext
-import org.jsoup.Jsoup
-import org.jsoup.nodes.Element
-import org.jsoup.select.Elements
-import org.slf4j.LoggerFactory
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
-import java.time.LocalDate
-import java.util.regex.Pattern
-import kotlin.time.measureTimedValue
 
 @Service
 class CrawlingService(
